18ddc3146f12f00d3efd8be057355e5e5029cc30
gogohhh <gogo_hhh_d@hotmail.com>
2021-10-22 12:29:32 -0500

    Squares array agregando x en Board

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import './App.css';
+import { useState } from 'react';
 import Board from './Board.js';
 import Square from './Square';
 
+const defaultSquares = () => (new Array(9)).fill(null);
+
 function App() {
+
+  const [squares, setSqueares] = useState(defaultSquares());
+
+  function handleSquareClick(index){
+    let newSquares = squares;
+    newSquares[index] = 'X';
+    setSqueares( [...newSquares]);
+  }
+
   return (
     <div>
       <main>
         <Board>
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
+          {squares.map((square, index) => 
+              
+              <Square 
+                x={square==='X' ? 1 : 0}
+                o={square==='O' ? 1 : 0}
+                onClick={ () => handleSquareClick(index)} />
+            )
+          }
         </Board>
       </main>
     </div>
--- src/Square.js
+++ src/Square.js
@@ -1,7 +1,9 @@
 
 function Square(props){
     return(
-        <div className="square"></div>
+        <div className={'square'} {...props} >
+            {props.x ? 'X' : (props.o ? 'O' : '') }
+        </div>
     );
 }
 
